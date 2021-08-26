e5d55ab1b40114da2eb294dd99b1dc19e7519098
eun <eb3020@naver.com>
2021-08-26 17:05:28 +0900

    은별작업중

---
+++ src/eb/Eun_test.java
@@ -0,0 +1,8 @@
+package eb;
+
+public class Eun_test {
+public void displ() {
+	System.out.println("조은별 : 기능 ");
+	
+}
+}
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,10 +2,13 @@ package main;
 
 import java.util.Scanner;
 
+import eb.Eun_test;
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int num;
+	Eun_test eb = new Eun_test();
 		while(true) {
 			System.out.println("1.한영수 2.진수 3.은별  4.김기범 5.유현준");
 			num = scan.nextInt();
@@ -15,6 +18,7 @@ public class MainClass {
 			case 2:
 				break;
 			case 3:
+				eb.displ();
 				break;
 			case 4:
 				break;
