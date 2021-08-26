9a20dda3737f026b512df0023c44d7fa56efe404
yhj <tntj123@naver.com>
2021-08-26 16:16:06 +0900

    플젝 생성

---
+++ src/main/MainClass.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1.한영수 2.진수 3.은별  4.김기범 5.유현준");
+			num = scan.nextInt();
+			switch(num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+				
+			}
+		}
+	}
+
+}
