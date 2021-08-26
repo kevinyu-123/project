29eb466b56ce4df00948e314d050d9ba6697e66c
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2021-08-26 16:52:28 +0900

    한영수 작업중

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,15 +2,21 @@ package main;
 
 import java.util.Scanner;
 
+import yeongsoo.TestYeongSoo;
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int num;
+		
+		TestYeongSoo tys = new TestYeongSoo();
+		
 		while(true) {
 			System.out.println("1.한영수 2.진수 3.은별  4.김기범 5.유현준");
 			num = scan.nextInt();
 			switch(num) {
 			case 1:
+				tys.aaaa();
 				break;
 			case 2:
 				break;
---
+++ src/yeongsoo/TestYeongSoo.java
@@ -0,0 +1,9 @@
+package yeongsoo;
+
+public class TestYeongSoo {
+
+	public void aaaa() {
+		System.out.println("한영수 프로젝트에서 aaaa기능 완료");
+	}
+
+}
